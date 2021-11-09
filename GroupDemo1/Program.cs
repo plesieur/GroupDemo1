5de693c6c71f93662e8ccd8cf7495306e578733e
Paul Lesieur <plesieur@jpkeefehs.org>
2021-11-09 07:47:42 -0500

    Add project files.

---
+++ GroupDemo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroupDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
